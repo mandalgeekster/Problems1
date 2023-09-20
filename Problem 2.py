0b1ef05c39c4ef98c3415413879d8a92a0b8db23
Bharathiv1403 <128215224+Bharathiv1403@users.noreply.github.com>
2023-09-20 11:07:11 +0530

    Problem 2. String Revesal

---
+++ Problem 2.py
@@ -0,0 +1,10 @@
+
+a=list(map(int,input().split(",")))
+arr=[]
+for i in a:
+    if i>=35:
+        arr.append("Pass")
+    else:
+        arr.append("Fail")
+print(arr)
+print("Finsh")/home/kabilan/Desktop/mohan/an.py
