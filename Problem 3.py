a77b64b5972b400c0fb25552ffb0495f1a99ee47
IamAayush2080 <ak2183874@gmail.com>
2023-10-30 19:58:27 +0545

    add problem 3.py

---
+++ Problem 3.py
@@ -0,0 +1,8 @@
+a = int(input("Enter value for a:"))
+b =int(input("Enter value for b:"))
+c = a+b
+print(c)
+if (c>2):
+    print("c is greater than 2.")
+else:
+    print("c is smaller than 2")    
